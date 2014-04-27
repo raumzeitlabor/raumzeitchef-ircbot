17e52de39622c7313ba9260b21bc40221b790be5
Maik Fischer <maikf@qu.cx>
2014-04-27 23:15:12 +0200

    Config: don't overwrite commandline args

--- lib/RaumZeitChef/Config.pm
+++ lib/RaumZeitChef/Config.pm
@@ -8,8 +8,16 @@ has 'config_filename' => (
     default => "$ENV{HOME}/.raumzeitchef.json",
 );
 
-has [qw/server port nick channel/] =>
-    (is => 'ro', required => 1);
+my %defaults = (
+    server => 'irc.hackint.net',
+    port => 6667,
+    nick => 'RaumZeitChef',
+    channel => '#raumzeitlabor',
+);
+
+for my $name (keys %defaults) {
+    has($name, is => 'ro', default => $defaults{$name});
+}
 
 has 'nickserv_password' => (is => 'ro');
 
@@ -19,14 +27,20 @@ sub BUILD {
     my ($self) = @_;
 
     return unless open my $fh, '<', $self->config_filename;
+
     my $config = decode_json(do { local $/; <$fh> });
+    close $fh;
 
     for my $key (keys %$config) {
         my $attr = $self->meta->get_attribute($key)
             or Carp::carp("'$key' is not a valid RaumZeitChef::Config attribute.");
 
-        $attr->set_value($self, $config->{$key});
+        # don't overwrite options supplied by the commandline
+        if ($self->$key eq $defaults{$key}) {
+            $attr->set_value($self, $config->{$key})
+        }
     }
+
 }
 
 1;
